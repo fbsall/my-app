de76eb31b19d8eafd7e23ac9015f81d637f98634
fbsall <fbsall@outlook.com>
2019-09-23 16:07:34 +0000

    Store states in the Board component instead of the individual Square components

--- src/index.js
+++ src/index.js
@@ -4,18 +4,12 @@ import './index.css';
 
 /* Render Single button */
 class Square extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      value : null,
-    };
-  }
   render() {
     return (
       <button className="square"
-      onClick={() => this.setState({value: 'X'})}
+      onClick={() => this.props.onClick()}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
@@ -23,8 +17,27 @@ class Square extends React.Component {
 
 /* Renders the Board with placeholder values */
 class Board extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      squares : Array(9).fill(null),
+    };
+
+  }
+
+  handleClick(i){
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value = {i}/>;
+    return (
+      <Square
+      value = {this.state.squares[i]}
+      onClick ={() => this.handleClick(i)}
+    />
+  );
   }
 
   render() {
