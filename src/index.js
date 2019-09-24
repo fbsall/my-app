f98d495ffcef6522f1e89d918e4e073ae5ea24aa
fbsall <fbsall@outlook.com>
2019-09-24 12:15:19 +0000

    player taking turns - determine which player goes next and the game’s state will be saved

--- src/index.js
+++ src/index.js
@@ -3,17 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 /* Render Single button */
-class Square extends React.Component {
-  render() {
+function Square(props) {
     return (
-      <button className="square"
-      onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
+      <button className="square" onClick={props.onClick}>
+        {props.value}
       </button>
     );
   }
-}
 
 /* Renders the Board with placeholder values */
 class Board extends React.Component {
@@ -21,14 +17,18 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares : Array(9).fill(null),
+      xIsNext: true,
     };
 
   }
 
   handleClick(i){
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext : !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -41,7 +41,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
